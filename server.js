7e7fbec09f49d3f0e48ab70fd4e3f8abc9ccbf50
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-05-01 12:49:38 -0700

    widgets work properly

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
-
+let db = require('./models')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -10,8 +10,51 @@ app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
 
+app.get('/',(req,res)=>{
+    db.widget.findAll()
+    .then(widgets=>{
+        res.render('home',{widgets})
+    })
+    .catch(err=>{
+        console.log(err)
+        res.render('error')
+      })
+   
+})
+
+app.post('/',(req,res)=>{
+    db.widget.findOrCreate({
+    where: {
+          description: req.body.description,
+          quantity: req.body.quantity,
+          },
+          defaults: req.body
+      })
+      .then(([widgets,created])=>{
+        res.redirect('/')
+      })
+      .catch(err=>{
+        console.log(err)
+        res.render('error')
+      })
+})
 
 
+app.delete('/',(req,res)=>{
+  db.widget.destroy({	
+    where: { 
+      description: req.body.description,
+      quantity: req.body.quantity 
+    }	
+  })
+  .then(deletedwid=>{
+    res.redirect('/')
+  })
+  .catch(err=>{
+    console.log(err)
+    res.render('error')
+  })	
+})
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
 app.listen(3000);
